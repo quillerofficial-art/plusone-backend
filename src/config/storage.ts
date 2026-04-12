c3da9afa90ed2f283925ee1c64c6b15843674930
quiller <quiller.official@gmail.com>
2026-04-12 17:26:54 +0530

    storage update

--- src/config/storage.ts
+++ src/config/storage.ts
@@ -4,10 +4,16 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const s3Client = new S3Client({
-  region: 'auto',
-  endpoint: process.env.BACKBLAZE_ENDPOINT!,
+  region: "auto",
+  endpoint: process.env.R2_ENDPOINT,
   credentials: {
-    accessKeyId: process.env.BACKBLAZE_KEY_ID!,
-    secretAccessKey: process.env.BACKBLAZE_APPLICATION_KEY!,
+    accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
   },
-})
+});
+
+console.log("CHECK CREDS:");
+console.log("ACCESS:", process.env.R2_ACCESS_KEY_ID);
+console.log("SECRET:", process.env.R2_SECRET_ACCESS_KEY);
+console.log("ENDPOINT:", process.env.R2_ENDPOINT);
+console.log("BUCKET:", process.env.R2_BUCKET_NAME);
